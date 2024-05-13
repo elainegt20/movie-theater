2558547cefed51cec3f7654df99c49c1b1ccf5a9
elainegt20 <elainegarcia@xyz-vgmxrlw17t.mynetworksettings.com>
2024-05-12 21:28:59 -0400

    server validation

--- controllers/showController.js
+++ controllers/showController.js
@@ -161,6 +161,55 @@ const deleteShow = async (req, res, next) => {
   }
 };
 
+//server validation
+
+const validateShowStatus = async (req, res, next) => {
+  const status = req.body.status;
+
+  //validate status
+  if (!status) {
+    return res.status(400).json({ message: "Status field is required." });
+  }
+
+  // Check for empty or whitespace-only strings
+  if (!status.trim()) {
+    return res
+      .status(400)
+      .json({ message: "Status cannot be empty or just whitespace." });
+  }
+
+  // Check for length
+  if (status.length < 5 || status.length > 25) {
+    return res
+      .status(400)
+      .json({ message: "Status must be between 5 and 25 characters long." });
+  }
+
+  // Validate rating
+  if (rating !== undefined) {
+    // Only validate rating if provided
+    if (typeof rating !== "number" || rating % 1 !== 0) {
+      return res.status(400).json({ message: "Rating must be an integer." });
+    }
+  }
+
+  next(); // Pass control to the next handler if validation is successful
+};
+
+// Validate rating
+
+const validateShowRating = async (req, res, next) => {
+  const rating = req.body.rating;
+  if (rating !== undefined) {
+    // Only validate rating if provided
+    if (typeof rating !== "number" || rating % 1 !== 0) {
+      return res.status(400).json({ message: "Rating must be an integer." });
+    }
+  } else {
+    return res.status(400).json({ message: "Rating must be provided." });
+  }
+};
+
 module.exports = {
   getAllShows,
   getOneShow,
@@ -168,4 +217,6 @@ module.exports = {
   updateRating,
   updateStatus,
   deleteShow,
+  validateShowStatus,
+  validateShowRating,
 };
